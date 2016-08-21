eeeef78c72573175152b39b1f9126bddfc002013
josephanthonycollins <joseph_anthony_collins@yahoo.co.uk>
2016-08-21 18:01:40 +0100

    First commit, adding in class templates and initial details for LoadData class.

---
+++ SocialNetWorkConsoleApp/SocialNetWorkConsoleApp/LoadData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SocialNetWorkConsoleApp
+{
+    public class LoadData
+    {
+        private List<String> originalData = new List<string>();
+
+        public List<String> OriginalData
+        {
+            get { return originalData; }
+            set { originalData = value; }
+        }
+
+        private string textFileLocation;
+
+        public string TextFileLocation
+        {
+            get { return textFileLocation; }
+            set { textFileLocation = value; }
+        }
+
+        public void load(string file)
+        {   
+            StreamReader sr = new StreamReader(file);
+
+            string tmp = "";            
+            do
+            {
+                tmp = sr.ReadLine();
+                OriginalData.Add(tmp);
+            }
+            while (tmp != null);
+            
+            sr.Close();
+        }
+
+        public bool getUserToEnterFileDetails()
+        {
+            Console.WriteLine("Enter file (i.e. C:\\SocialNetwork.txt): ");
+            //string fname = Console.ReadLine();
+            string fname = "D:\\JC Masters\\UTRC Problem\\SocialNetwork.txt";
+
+            this.TextFileLocation = fname;
+
+            bool fileExists = checkFileExists(this.TextFileLocation);
+            
+            if (fileExists)
+                return true;
+
+            return false;
+        }
+
+        public bool checkFileExists(string file)
+        {
+            FileInfo f = new FileInfo(file);
+            if (f.Exists)
+                return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ SocialNetWorkConsoleApp/SocialNetWorkConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetWorkConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LoadData dataLoader = new LoadData();
+            bool doesFileExist = dataLoader.getUserToEnterFileDetails();
+
+            if (doesFileExist)
+                dataLoader.load(dataLoader.TextFileLocation);
+            else
+            {
+                Console.WriteLine("\n\nFile does not exist. Exiting.");
+                return;
+            }
+
+
+        }
+    }
+}
