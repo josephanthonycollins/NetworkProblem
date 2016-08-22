8101a8ee796f39f6ba4ed68bd9c682699e8b39dd
josephanthonycollins <joseph_anthony_collins@yahoo.co.uk>
2016-08-22 21:36:45 +0100

    Adding SimulationWrapper class to reduce complexity of Main().

--- SocialNetWorkConsoleApp/SocialNetWorkConsoleApp/Program.cs
+++ SocialNetWorkConsoleApp/SocialNetWorkConsoleApp/Program.cs
@@ -9,6 +9,8 @@ namespace SocialNetWorkConsoleApp
     {
         static void Main(string[] args)
         {
+            //iteration upper bound
+            const int upperbound = 15;
             //Loading the Data
             LoadData dataLoader = new LoadData();
             bool doesFileExist = dataLoader.getUserToEnterFileDetails();
@@ -21,10 +23,69 @@ namespace SocialNetWorkConsoleApp
                 return;
             }
 
-            //Parsing the Original Data to Unique List of Nodes
+            //Parsing the Original Data to get unique list of names and indices
             ParseData datafilter = new ParseData();
-            bool canwefilter = datafilter.generateUniqueNamesAndIndices(dataLoader.OriginalData);
+            bool canWeFilter = datafilter.generateUniqueNamesAndIndices(dataLoader.OriginalData);
+
+            if (!canWeFilter)
+            {
+                Console.WriteLine("\n\nCan't generate List of names. Exiting.");
+                return;   
+            }
+
+            //Now we generate a n by n Matrix where n is the number of unqiue nodes, all entries are 0
+            Matrix matrix = new Matrix(datafilter.UniqueListOfNodes.Count, datafilter.UniqueListOfNodes.Count(), 0.0);
+            
+            //Populate the matrix, 1 indicates two Nodes are connected
+            for (int i = 0; i < datafilter.IndexI.Count; i++)
+            {
+                matrix[datafilter.IndexI[i], datafilter.IndexJ[i]] = 1.0;
+                //matrix will be symmetric
+                matrix[datafilter.IndexJ[i],datafilter.IndexI[i]] = 1.0;
+            }
+
+            //Test check if element 1 and element 400 are connected
+            int iteration = upperbound;
+
+            while (iteration > 0)
+            {
+                //TODO: change this so that we can enter in any 
+                if (matrix[1 - 1, 400 - 1] == 1.0)
+                {
+                    Console.WriteLine("\n\nElement 1 and Element 400 linked after {0} iterations, exiting.",upperbound-iteration);
+                }
+                iteration--;
+
+                matrix = matrix * matrix;
+            }
+
+            if (iteration == 0)
+            {
+                Console.WriteLine("\n\nElement 1 and Element 400 are not linked after 15 iterations.Exiting");
+                return;
+            }
 
         }
     }
+
+    //Class used to wrap the matrix calculations i.e. simplify the Main() body
+
+    public class MatrixWrapper
+    {
+        private Matrix _matrix = null;
+
+        internal Matrix _Matrix
+        {
+            get { return _matrix; }
+            set { _matrix = value; }
+        }
+        private ParseData _data = null;
+
+        internal ParseData Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+    }
+
 }
